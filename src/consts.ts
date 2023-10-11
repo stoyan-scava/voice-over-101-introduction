52474b1bc9043d2d0d551ef95ebcf08740c72c5a
stoyan-scava <stoyan.georgiev.ext@docebo.com>
2023-10-11 22:42:01 +0300

    feat: audio for overview and first chapter

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Introduction to Voice-Over Narration';
+export const SITE_DESCRIPTION = 'This course will focus on developing the 💪skills and 🎓knowledge necessary for performing voice-over work.';
